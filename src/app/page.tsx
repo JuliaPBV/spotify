7b2fdacd16003221af25514f5fc2dcc4c0370198
Julia Pereira <vital.pereira.julia@gmail.com>
2025-03-20 11:10:34 -0300

    finalização

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,61 +1,13 @@
-import {
-  Home as HomeIcon,
-  Search,
-  Library,
-  ChevronLeft,
-  ChevronRight,
-  Play,
-  Shuffle,
-  SkipBack,
-} from "lucide-react";
+import { Footer } from "@/components/Footer";
+import { Sidebar } from "@/components/Sidebar";
+import { ChevronLeft, ChevronRight, Play } from "lucide-react";
 import Image from "next/image";
 export default function Home() {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
-        <aside className="w-72 bg-zinc-950 p-9">
-          <div className="flex items-center gap-2">
-            <div className="w-3 h-3 bg-red-500 rounded-full" />
-            <div className="w-3 h-3 bg-yellow-500 rounded-full" />
-            <div className="w-3 h-3 bg-green-500 rounded-full" />
-          </div>
+        <Sidebar />
 
-          <nav className="space-y-5 mt-10">
-            <a
-              href=""
-              className="flex items-center gap-3 text-sm font-semibold text-zinc-200"
-            >
-              <HomeIcon />
-              Home
-            </a>
-            <a
-              href=""
-              className="flex items-center gap-3 text-sm font-semibold text-zinc-200"
-            >
-              <Search />
-              Search
-            </a>
-            <a
-              href=""
-              className="flex items-center gap-3 text-sm font-semibold text-zinc-200"
-            >
-              <Library />
-              Your Library
-            </a>
-          </nav>
-
-          <nav className="mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-2">
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
-              Hot Hits Brasil
-            </a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
-              Top Brasil
-            </a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
-              Favoritas
-            </a>
-          </nav>
-        </aside>
         <main className="flex-1 p-6">
           <div className="flex items-center gap-4">
             <button className="rounded-full bg-black/40 p-1">
@@ -236,28 +188,7 @@ export default function Home() {
           </div>
         </main>
       </div>
-      <footer className="bg-zinc-800 botder-t border-zinc-700 p-6 flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <Image
-            src="/capa.jpg"
-            width={56}
-            height={56}
-            alt="capa album homem pixando algo"
-          />
-          <div className="flex flex-col gap-3">
-            <strong className="font-normal">Da onde eu venho</strong>
-            <span className="text-xs text-zinc-400">Filipe Ret</span>
-          </div>
-        </div>
-        <div className="flex flex-col items-center">
-          <div>
-            <Shuffle />
-            <SkipBack />
-            <Play />
-          </div>
-        </div>
-        <div></div>
-      </footer>
+      <Footer />
     </div>
   );
 }
